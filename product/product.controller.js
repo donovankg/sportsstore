e8d71f4d31aed47927f2f9aa0bcebb0d85c09854
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-09-27 16:54:33 -0500

    Add product controller

--- product/product.controller.js
+++ product/product.controller.js
@@ -1,3 +1,19 @@
 /**
  * Created by cleverjam on 9/27/16.
  */
+
+var Product = require("./product.model");
+var interface = {
+    getList: function getList(){
+        return new Promise(function(resolve,reject){
+            Product.find(function(err, products){
+                if(err)
+                    reject(err);
+                resolve(products)
+            })
+        });
+    }
+}
+
+
+module.exports = interface;
