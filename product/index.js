d9dd7b812d12a27010a479bd36ec7acfbc47e508
Jaime Liz <jaime.liz@ksquarelabs.com>
2016-09-27 16:54:47 -0500

    Add main product endpoint

--- product/index.js
+++ product/index.js
@@ -3,9 +3,17 @@
  */
 var express = require("express");
 var router = express.Router();
+var Product = require("./product.controller");
 
 router.get("/", function(req, res, next){
-    res.json({"product index":"ok"})
+    Product.getList()
+        .then(function(products){
+            res.type("json");
+            res.json(products)
+        })
+        .catch(function(e){
+            res.send(e);
+        })
 })
 
 module.exports = router;
