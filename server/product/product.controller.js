a2c78f5860428a366e805b45ef6433b42f2007a4
lchapoy <luis_chapoy@hotmail.com>
2016-09-27 19:15:28 -0500

    add product CRUD api

--- server/product/product.controller.js
+++ server/product/product.controller.js
@@ -10,6 +10,28 @@ var interface = {
                 console.log(err);
                 res.status(400).send(err);
             });
+    },
+    save: function(req,res,next){
+        var id = req.params.id;
+        Product.update({_id:id},req.body)
+            .then(function(products){
+                res.send(products)
+            })
+            .catch(function(err){
+                console.log(err);
+                res.status(400).send(err);
+            });
+    },
+    delete: function(req,res,next){
+        var id = req.params.id;
+        Product.find({_id:id}).remove()
+            .then(function(){
+                res.send("product deleted")
+            })
+            .catch(function(err){
+                console.log(err);
+                res.status(400).send(err);
+            });
     }
 };
 
