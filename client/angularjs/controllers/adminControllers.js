87fadced5a9a16bb2e6d3e7f409c03ebb8435db2
Donovan Goldston <donovan.goldston@Ksquarelabs.com>
2016-09-29 23:04:57 -0500

    admin view orders is working correctly and products are showing in the admin page, you can delete but edit and new don't save

--- client/angularjs/controllers/adminControllers.js
+++ client/angularjs/controllers/adminControllers.js
@@ -1,5 +1,6 @@
 angular.module("sportsStoreAdmin")
     .constant("authUrl", "http://localhost:3000/users/login")
+    .constant("ordersUrl", "http://localhost:3000/orders")
     .controller("authCtrl", function($http, $location, authUrl) {
         var self = this;
 
@@ -15,16 +16,40 @@ angular.module("sportsStoreAdmin")
             });
         }
     })
-.controller("mainCtrl", function() {
-    this.screens = ["Products", "Orders"];
-    this.current = this.screens[0];
+    .controller("mainCtrl", function() {
+        this.screens = ["Products", "Orders"];
+        this.current = this.screens[0];
 
-    this.setScreen = function(index){
-      this.current = this.screens[index];
-    };
-this.getScreen = function() {
-return this.current == "Products" ?
-    "angularjs/views/adminProducts.html" :
-    "angularjs/views/adminOrders.html";
-  };
-});
+        this.setScreen = function(index) {
+            this.current = this.screens[index];
+        };
+        this.getScreen = function() {
+            return this.current == "Products" ?
+                "angularjs/views/adminProducts.html" :
+                "angularjs/views/adminOrders.html";
+        };
+    })
+
+    .controller("ordersCtrl", function($http, ordersUrl) {
+      var self = this;
+
+        $http.get(ordersUrl)
+            .then(function(res) {
+                self.orders = res.data;
+            })
+            .catch(function(error) {
+                error = error;
+            });
+            self.selectedOrder;
+        self.selectOrder = function(order) {
+            this.selectedOrder = order;
+        };
+
+        self.calcTotal = function(order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                total += order.products[i].count * order.products[i].price;
+            }
+            return total;
+        }
+    })
---
+++ client/angularjs/controllers/adminProductController.js
@@ -0,0 +1,36 @@
+angular.module("sportsStoreAdmin")
+    .constant("productUrl", "http://localhost:3000/products/")
+    .config(function($httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+    })
+    .controller("productCtrl", function($resource, productUrl) {
+        var self = this;
+        this.productsResource = $resource(productUrl + ":id", {
+            id: "@_id"
+        });
+        this.listProducts = function() {
+            this.products = this.productsResource.query();
+        }
+        this.deleteProduct = function(product) {
+            product.$delete().then(function() {
+                self.products.splice(self.products.indexOf(product), 1);
+            });
+        }
+        this.createProduct = function(product) {
+            new self.productsResource(product).$save().then(function(newProduct) {
+                self.products.push(newProduct);
+                self.editedProduct = null;
+            });
+        }
+        this.updateProduct = function(product) {
+            product.$save();
+            this.editedProduct = null;
+        }
+        this.startEdit = function(product) {
+            this.editedProduct = product;
+        }
+        this.cancelEdit = function() {
+            this.editedProduct = null;
+        }
+        this.listProducts();
+    });
